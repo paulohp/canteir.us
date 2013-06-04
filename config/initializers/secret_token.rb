60cb52fc2fe8e8866ae06a07224cdfff325e9f99
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-03 22:41:41 -0300

    init this

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canteiro::Application.config.secret_token = 'ffbba9165401a29f4072f2b87a27c74b6e7db5163259796e2be2ba6fcd8e34ae4af5bcca307378c5401be535c651fc75fd7cea7df360564116a50e1d4b958d6a'
