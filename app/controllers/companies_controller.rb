f8751db941b9a7f26a6aca7a2121b88036a67c8b
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-04 23:13:32 -0300

    gambis a vista

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,3 @@
 class CompaniesController < InheritedResources::Base
+  before_filter :authenticate_company!, :except => [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130605015642) do
+ActiveRecord::Schema.define(:version => 20130605021032) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
     t.string   "location"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "companies", ["email"], :name => "index_companies_on_email", :unique => true
+  add_index "companies", ["reset_password_token"], :name => "index_companies_on_reset_password_token", :unique => true
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
