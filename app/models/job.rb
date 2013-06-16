92e36c2a4cd3a12f7805809e5891c420f7349495
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-16 13:50:28 -0300

    refatorando metodo de busca

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,11 +2,7 @@ class Job < ActiveRecord::Base
   belongs_to :companies
   attr_accessible :category, :description, :location, :title
 
-  def self.search search
-    if search
-      where 'name LIKE ?', "%#{search}%"
-    else
-      scoped
-    end
+  def self.search(query)
+    where("title like ?", "%#{query}%")
   end
 end
