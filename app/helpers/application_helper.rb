671b683c3bd6ffcb4d6109b061c679c72baa0504
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-17 23:39:33 -0300

    resolving issue #2 and issue #3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_to_resume resume
+    if resume == nil
+      "/users/#{current_user.id}/resumes/new"
+    else
+      "/users/#{current_user.id}/resumes/#{current_user.resume.id}"
+    end
+  end
 end
