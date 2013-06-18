c9964cabbabfccbe5ec7cd2f7544617bb9ac9991
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-18 17:55:45 -0300

    resolvendo problemas de relacionamento

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,50 @@
 class ResumesController < InheritedResources::Base
+
+  def new
+    @user = current_user
+    @resume = @user.build_resume
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @resume }
+    end
+  end
+
+  # GET /tasks/1/edit
+  def edit
+    @resume = current_user.resume.find(params[:id])
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @user = current_user
+    @resume = @user.create_resume(params[:resume])
+
+    respond_to do |format|
+      if @resume.save
+        format.html { redirect_to @resume, notice: 'Task was successfully created.' }
+        format.json { render json: @resume, status: :created, location: @resume }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @resume.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tasks/1
+  # PUT /tasks/1.json
+  def update
+    @resume = current_user.resumes.find(params[:id])
+
+    respond_to do |format|
+      if @resume.update_attributes(params[:resume])
+        format.html { redirect_to @resume, notice: 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @resume.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
