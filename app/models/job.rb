8ec4b27077c6ecf163b50e227e10131c3e2ac200
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-15 23:29:26 -0300

    melhorando a view show de jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ActiveRecord::Base
   belongs_to :company
   has_many :applies, dependent: :destroy
   has_many :applied_jobs, :through => :applies, :source => :job
-  attr_accessible :category, :description, :location, :title, :resume,:resume_id
+  attr_accessible :category, :description, :location, :title, :resume,:resume_id, :role, :salary
 
   def self.search(query)
     where("title like ?", "%#{query}%")
