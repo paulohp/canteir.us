865fcb3066a7a754c75b3d6e0b816fc9ce5c033f
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-15 21:45:48 -0300

    arrumando o link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,17 +39,21 @@ module ApplicationHelper
 
   def ejob_applied? ejob_id
     if user_signed_in?
-      applicados = current_user.resume.applies
-      if applicados == []
-        return link_to 'Aplicar', "/jobs/#{ejob_id}/applies/new", :class => 'btn btn-block btn-success'
-      else
-        applicados.each do |aplicado|
-          unless aplicado.job_id == ejob_id
-            return link_to 'Aplicar', "/jobs/#{ejob_id}/applies/new", :class => 'btn btn-block btn-success'
-          else
-            return link_to 'Já Enviada', "#", :class => 'btn btn-block btn-danger'
+      if current_user.resume
+        applicados = current_user.resume.applies
+        if applicados == []
+          return link_to 'Aplicar', "/jobs/#{ejob_id}/applies/new", :class => 'btn btn-block btn-success'
+        else
+          applicados.each do |aplicado|
+            unless aplicado.job_id == ejob_id
+              return link_to 'Aplicar', "/jobs/#{ejob_id}/applies/new", :class => 'btn btn-block btn-success'
+            else
+              return link_to 'Já Enviada', "#", :class => 'btn btn-block btn-danger'
+            end
           end
         end
+      else
+        return link_to 'Completar Curriculo', "#", :class => 'btn btn-block btn-danger'
       end
     else
       return link_to 'Aplicar', "/jobs/#{ejob_id}/applies/new", :class => 'btn btn-block btn-success'
