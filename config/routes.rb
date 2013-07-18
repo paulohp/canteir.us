49a4e51498dca9a45176714898eed309a05a87f8
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-18 18:58:00 -0300

    mudando cor do index e root no routes

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ Canteiro::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'jobs#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
