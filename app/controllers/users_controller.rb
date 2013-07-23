fd5eef56fcb2d0b6937ae2ee17d78365f0a67990
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-23 14:24:37 -0300

    arrumando alguns metodos nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < InheritedResources::Base
 	def show
     @user = User.find(params[:id])
-    @vagas_aplicadas = @user.resume.applies
+    @vagas_aplicadas = @user.resume.applies unless @user.resume.nil?
 	end
 end
