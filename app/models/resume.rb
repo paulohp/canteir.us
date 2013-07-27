f4f24c739d8a82bd6f39c2bd0af400289e9e8007
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-27 00:23:10 -0300

    resume styleee

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,5 +2,5 @@ class Resume < ActiveRecord::Base
   belongs_to :user
   has_many :applies
   has_many :jobs
-  attr_accessible :children, :civil, :dob, :gender, :job_area, :mini_resume, :name, :professional_objctive, :user_id
+  attr_accessible :cpf, :country, :state, :city, :district, :address, :complement, :postal_code, :tel_residence, :scraps, :tel_celular, :tel_commercial, :fax, :deficience,:children, :civil, :dob, :gender, :job_area, :mini_resume, :name, :professional_objctive, :user_id
 end
